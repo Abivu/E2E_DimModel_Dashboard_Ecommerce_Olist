0a0f7ddb23d7bf31e63f51274751be4a2e00cee4
Abi Vu <vu.ab.159@gmail.com>
2024-02-24 17:54:02 -0600

    Removed Lat/Lng in cust/seller-preprocess prod/factorder-added Geo-restructured factreview

--- Scripts/transform_load_scripts.py
+++ Scripts/transform_load_scripts.py
@@ -4,26 +4,19 @@ DimCustomer = """
                                                     CustomerState, 
                                                     CustomerCity, 
                                                     CustomeZIPCode, 
-                                                    CustomeLat, 
-                                                    CustomeLng, 
                                                     ValidFrom, 
                                                     ValidTo, 
                                                     IsCurrent)
                 SELECT 
-                    cust.customer_id,
-                    cust.customer_unique_id,
-                    cust.customer_state,
-                    cust.customer_city,
-                    cust.customer_zip_code_prefix,
-                    geo.geolocation_lat,
-                    geo.geolocation_lng,
+                    customer_id,
+                    customer_unique_id,
+                    customer_state,
+                    customer_city,
+                    customer_zip_code_prefix,
                     now(),
                     '9999-12-31 23:59:59',
                     1
-                FROM olist_oltp.olist_customers_dataset cust 
-                LEFT JOIN olist_oltp.olist_geolocation_dataset geo
-                ON cust.customer_zip_code_prefix = geo.geolocation_zip_code_prefix
-                AND cust.customer_city = geo.geolocation_city
+                FROM olist_oltp.olist_customers_dataset 
                 LIMIT %s OFFSET %s ;
 """
 DimCustomer_script = """SELECT COUNT(*) FROM olist_oltp.olist_customers_dataset"""
@@ -34,8 +27,6 @@ DimSeller = """
                                                     SellerState, 
                                                     SellerCity, 
                                                     SellerZIPCode, 
-                                                    SellerLat, 
-                                                    SellerLng, 
                                                     ValidFrom, 
                                                     ValidTo, 
                                                     IsCurrent)
@@ -44,15 +35,10 @@ DimSeller = """
                     sel.seller_state,
                     sel.seller_city,
                     sel.seller_zip_code_prefix,
-                    geo.geolocation_lat,
-                    geo.geolocation_lng,
                     now(),
                     '9999-12-31 23:59:59',
                     1
                 FROM olist_oltp.olist_sellers_dataset sel
-                LEFT JOIN olist_oltp.olist_geolocation_dataset geo
-                ON sel.seller_zip_code_prefix = geo.geolocation_zip_code_prefix
-                AND sel.seller_city = geo.geolocation_city
                 LIMIT %s OFFSET %s ;
 """
 DimSeller_script = """SELECT COUNT(*) FROM olist_oltp.olist_sellers_dataset"""
@@ -75,12 +61,12 @@ DimProduct = """
                     prod.product_id,
                     prod.product_category_name,
                     cat.product_category_name_english,
-                    prod.product_name_lenght,
-                    prod.product_photos_qty,
-                    prod.product_weight_g,
-                    prod.product_length_cm,
-                    prod.product_height_cm,
-                    prod.product_width_cm,
+                    COALESCE(CHAR_LENGTH(prod.product_name_lenght)>1, 0) AS product_name_lenght,
+                    COALESCE(CHAR_LENGTH(prod.product_photos_qty)>1, 0) AS product_photos_qty,
+                    COALESCE(CHAR_LENGTH(prod.product_weight_g)>1, 0) AS product_weight_g,
+                    COALESCE(CHAR_LENGTH(prod.product_length_cm)>1, 0) AS product_length_cm,
+                    COALESCE(CHAR_LENGTH(prod.product_height_cm)>1, 0) AS product_height_cm,
+                    COALESCE(CHAR_LENGTH(prod.product_width_cm)>1, 0) AS product_width_cm,
                     now(),
                     '9999-12-31 23:59:59',
                     1
@@ -169,47 +155,67 @@ DimDate_script = """SELECT COUNT(DISTINCT(Date_format(date_time, '%Y%m%d')))
         SELECT order_estimated_delivery_date AS date_time FROM olist_oltp.olist_orders_dataset) main;"""
 
 
+DimGeolocation = """
+                INSERT INTO olist_olap.DimGeolocation( GeoZipCode,
+                                                        GeoLat,
+                                                        GeoLng,
+                                                        GeoCity,
+                                                        GeoState)
+                SELECT 
+                    geolocation_zip_code_prefix,
+                    geolocation_lat,
+                    geolocation_lng,
+                    geolocation_city,
+                    geolocation_state
+                FROM olist_oltp.olist_geolocation_dataset
+                LIMIT %s OFFSET %s;"""
+
+DimGeolocation_script = """SELECT COUNT(*) FROM olist_oltp.olist_geolocation_dataset;"""
+
 FactOrderItem = """
                 INSERT INTO olist_olap.FactOrderItem( OrderItemID,
-                                                    OrderID,
-                                                    ProdKey,
-                                                    SellerKey,
-                                                    CustomerKey,
-                                                    price,
-                                                    FreightValue,
-                                                    StatusKey,
-                                                    DateKey,
-                                                    PayMethodKey,
-                                                    PaymentValue,
-                                                    PurchaseTimestamp,
-                                                    ApprovedAt,
-                                                    ShippingLimitDate,
-                                                    DeliveredCarrierDate,
-                                                    DeliveredCustomerDate,
-                                                    EstimatedDeliverDate,
-                                                    Vversion)
+                                                        OrderID,
+                                                        ProdKey,
+                                                        SellerKey,
+                                                        CustomerKey,
+                                                        price,
+                                                        FreightValue,
+                                                        OrderStatusKey,
+                                                        DateKey,
+                                                        PayMethodKey,
+                                                        PaymentValue,
+                                                        PurchaseTimestamp,
+                                                        ApprovedAt,
+                                                        ShippingLimitDate,
+                                                        DeliveredCarrierDate,
+                                                        DeliveredCustomerDate,
+                                                        EstimatedDeliverDate,
+                                                        Vversion)
                 SELECT 
-                    CONCAT(oi.order_id, '-', oi.order_item_id) AS OrderItemID,
-                    oi.order_id,
-                    prod.ProdKey,
-                    sell.SellerKey,
-                    cust.CustomerKey,
-                    oi.price,
-                    oi.freight_value,
-                    stat.StatusKey,
-                    pay.PayMethodKey,
-                    pay.payment_value,
-                    ord.order_purchase_timestamp,
-                    ord.order_approved_at,
-                    oi.shipping_limit_date,
-                    ord.order_delivered_carrier_date,
-                    ord.order_delivered_customer_date,
-                    ord.order_estimated_delivery_date,
-                    1
+                        CONCAT(oi.order_id, '-', oi.order_item_id) AS OrderItemID,
+                        oi.order_id,
+                        prod.ProdKey,
+                        sell.SellerKey,
+                        cust.CustomerKey,
+                        oi.price,
+                        oi.freight_value,
+                        stat.OrderStatusKey,
+                        IF(CHAR_LENGTH(order_purchase_timestamp)>5, DATE_FORMAT(order_purchase_timestamp, '%Y%m%d'), NULL),
+                        pay.PayMethodKey,
+                        paym.payment_value,
+                        IF(CHAR_LENGTH(ord.order_purchase_timestamp) > 5, CAST(ord.order_purchase_timestamp AS DATETIME), NULL),
+                        IF(CHAR_LENGTH(ord.order_approved_at) > 5, CAST(ord.order_approved_at AS DATETIME), NULL),
+                        IF(CHAR_LENGTH(oi.shipping_limit_date) > 5, CAST(oi.shipping_limit_date AS DATETIME), NULL),
+                        IF(CHAR_LENGTH(ord.order_delivered_carrier_date) > 5, CAST(ord.order_delivered_carrier_date AS DATETIME), NULL),
+                        IF(CHAR_LENGTH(ord.order_delivered_customer_date) > 5, CAST(ord.order_delivered_customer_date AS DATETIME), NULL),
+                        IF(CHAR_LENGTH(ord.order_estimated_delivery_date) > 5, CAST(ord.order_estimated_delivery_date AS DATETIME), NULL),
+                        1
 
                 FROM olist_oltp.olist_order_items_dataset oi
                 LEFT JOIN olist_oltp.olist_orders_dataset ord
                 ON oi.order_id = ord.order_id
+                LEFT JOIN olist_oltp.olist_order_payments_dataset paym
+                ON ord.order_id = paym.order_id
                 LEFT JOIN olist_olap.DimCustomer cust
                 ON ord.customer_id = cust.CustomerID
                 LEFT JOIN olist_olap.DimOrderStatus stat
@@ -218,8 +224,8 @@ FactOrderItem = """
                 ON oi.product_id = prod.ProdID
                 LEFT JOIN olist_olap.DimSeller sell
                 ON oi.seller_id = sell.SellerID
-                LEFT JOIN olist_oltp.olist_order_payments_dataset pay
-                ON ord.order_id = pay.order_id
+                LEFT JOIN olist_olap.DimPayMethod pay
+                ON paym.payment_type = pay.PaymentType
                 LIMIT %s OFFSET %s;
 """
 FactOrderItem_script = """SELECT COUNT(*) FROM olist_oltp.olist_order_items_dataset;"""
@@ -228,33 +234,22 @@ FactOrderItem_script = """SELECT COUNT(*) FROM olist_oltp.olist_order_items_data
 FactOrderReview = """
                 INSERT INTO olist_olap.FactOrderReview( ReviewID,
                                                     OrderID,
-                                                    CustomerKey,
-                                                    SellerKey,
                                                     ReviewScore,
                                                     ReviewTitle,
                                                     ReviewMsg,
                                                     CreationDate,
                                                     ReviewTimestamp)
-                SELECT 
-                    rev.review_id,
-                    rev.order_id,
-                    cust.CustomerKey,
-                    sell.SellerKey,
-                    rev.review_score,
-                    rev.review_comment_title,
-                    rev.review_comment_message,
-                    rev.review_creation_date,
-                    rev.review_answer_timestamp                
-
-                FROM olist_oltp.olist_order_reviews_dataset rev
-                LEFT JOIN olist_oltp.olist_orders_dataset ord
-                ON rev.order_id = ord.order_id
-                left join olist_olap.DimCustomer cust
-                ON ord.customer_id = cust.CustomerID
-                LEFT JOIN olist_oltp.olist_order_items_dataset oi
-                ON ord.order_id = oi.order_id
-                LEFT JOIN olist_olap.DimSeller sell
-                ON oi.seller_id = sell.SellerID
+                SELECT   review_id
+                        ,order_id
+                        ,review_score
+                        ,review_comment_title
+                        ,review_comment_message
+                        ,IF(CHAR_LENGTH(review_creation_date) > 5, CAST(review_creation_date AS DATETIME), NULL)
+                        ,IF(CHAR_LENGTH(review_answer_timestamp) > 5, CAST(review_answer_timestamp AS DATETIME), NULL)  
+                FROM
+                        (SELECT *, ROW_NUMBER() OVER(PARTITION BY order_id ORDER BY review_answer_timestamp DESC) AS row_num 
+                        FROM olist_oltp.olist_order_reviews_dataset) main
+                WHERE row_num = 1
                 LIMIT %s OFFSET %s;
 """
 
